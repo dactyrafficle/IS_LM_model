089aa6ecc3334bcabb1f63b31e6ee9e3becc496e
dactyrafficle <40529986+dactyrafficle@users.noreply.github.com>
2021-07-18 00:28:05 -0400

    Delete GET_SOLUTION.js

--- GET_SOLUTION.js
+++
@@ -1,7 +0,0 @@
-function GET_SOLUTION(obj) {
- // Y VIA THE LM CURVE - DEPENDS ON i NOT r
- let A = (obj.M/(obj.gamma*obj.P))**(1/obj.epsilon);
- let Y = A*obj.i**(obj.theta/obj.epsilon);
- //console.log(Y);
- return GETEIS(obj, Y);
-}
